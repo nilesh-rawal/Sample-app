27091c9d7758bbadd4e46eae9bc19024a61bd0c3
Nilesh Kumar Rawal <rawalnil007@gmail.com>
2020-08-25 13:33:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask 
-app = Flask(__name__) 
-  
-@app.route('/') 
-def hello(): 
-    return "welcome to the flask tutorials"
+app = Flask(__name__)   
+
+@app.route("/")
+def main():
+    return "Welcome!! Nilesh.. How are you doing"
+
+@app.route('/how are you')
+def hello():
+    return 'I am good, how about you?'
   
   
 if __name__ == "__main__": 
